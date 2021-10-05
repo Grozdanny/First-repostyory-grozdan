91032d28ca8fdee5c82bed89430cc80fccaca6af
Grozdanny <marcinzur277@gmail.com>
2021-10-05 11:45:16 +0000

    Added Input

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = (
+const Normal = (
     <h1>Hello World!</h1>
 )
+class Hello extends React.Component{
+    constructor(props) {
+        super(props);
 
-ReactDOM.render(element, document.getElementById('root'));
+        this.state = {
+            name: "World"
+        }
+    }
+
+    render() {
+        return <> 
+        <h1>Hello {this.state.name} </h1>
+        <input type="text" onChange={(event) => this.change(event)} />
+        </>
+    }
+    change(event){
+        console.log(event.target.value);
+        this.setState({name: event.target.value});
+    }
+}
+ const Box = ( <>
+    {Normal}
+    <hr/>
+    <Hello/>
+    </>
+ )
+ReactDOM.render(Box, document.getElementById('root'));
