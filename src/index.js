1e27d05863cad4ab14dc8783e54c2c35c85ba9b4
Grozdanny <marcinzur277@gmail.com>
2021-10-06 08:39:09 +0000

    Added clock

--- src/index.js
+++ src/index.js
@@ -9,12 +9,22 @@ class Hello extends React.Component{
         super(props);
 
         this.state = {
-            name: "World"
-        }
+            name: "World",
+            date: new Date(),
+        };
     }
+    tick() {
+        this.setState({ date: new Date() });
+      }
+    componentDidMount() {
+        setInterval(() => {
+          this.tick();
+        }, 1000);
+      }
 
     render() {
         return <> 
+        <h1>It's {this.state.date.toLocaleTimeString()}</h1>
         <h1>Hello {this.state.name} </h1>
         <input type="text" onChange={(event) => this.change(event)} />
         </>
